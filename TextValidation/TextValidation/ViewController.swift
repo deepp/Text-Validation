f95f84c783ff739826c731c8067ebe71a9a4b611
dilipchaurasiya <dilipchaurasiya@yahoo.com>
2015-10-13 19:33:56 +0530

    done validation showing message changed alert text with alert placeholder having red color

--- TextValidation/TextValidation/ViewController.swift
+++ TextValidation/TextValidation/ViewController.swift
@@ -8,49 +8,96 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     let status: Bool = true
+    var flag:Bool = false
 
     @IBOutlet weak var PropertyInput: UITextField!
-    @IBOutlet weak var r1: UITextField!
-    @IBOutlet weak var r2: UITextField!
+   
+    //@IBOutlet weak var Propertyinput: UITextField!
+    @IBOutlet weak var renttext: UITextField!
     
-    @IBOutlet weak var datepicker: UITextField!
+    @IBOutlet weak var Deposittext: UITextField!
+    
+    @IBOutlet weak var Datepicker: UITextField!
     @IBAction func datepicker(sender: AnyObject) {
         
         let DatePickerView : UIDatePicker = UIDatePicker()
         DatePickerView.datePickerMode = UIDatePickerMode.Date
-        datepicker.inputView = DatePickerView
+        Datepicker.inputView = DatePickerView
         DatePickerView.addTarget(self, action: Selector("handleDatePicker:"), forControlEvents: UIControlEvents.ValueChanged)
     }
+    
     func handleDatePicker(sender: UIDatePicker) {
         let dateFormatter = NSDateFormatter()
         dateFormatter.dateFormat = "dd MMM yyyy"
-        datepicker.text = dateFormatter.stringFromDate(sender.date)
+        Datepicker.text = dateFormatter.stringFromDate(sender.date)
+        
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        PropertyInput.clearsOnBeginEditing = true
+        renttext.clearsOnBeginEditing = true
+        Deposittext.clearsOnBeginEditing = true
+        Datepicker.clearsOnBeginEditing = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func submit(sender: AnyObject) {
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        //var clearsOnBeginEditing: Bool = true
         
-        if((PropertyInput.text?.isEmpty == status)){
+        textField.textColor = UIColor.blackColor()
+        
+    }
+
+
+    @IBAction func submit(sender: AnyObject)
+    {
+        
+        if(PI() == status)
+        {
             
            // PropertyInput.layer.borderColor = UIColor.redColor().CGColor
-            PropertyInput.text = "Enter Property";
-            PropertyInput.textColor = UIColor.redColor();
-            
+           // PropertyInput.text = "Enter Property"
+            PropertyInput.attributedPlaceholder = NSAttributedString(string:"Enter Property",
+                attributes:[NSForegroundColorAttributeName: UIColor.redColor()])
         }
-        else
+             if(RT() == status)
+             {
+               // renttext.text = "Enter Rent"
+               // renttext.textColor = UIColor.redColor()
+                renttext.attributedPlaceholder = NSAttributedString(string:"Enter Rent",
+                    attributes:[NSForegroundColorAttributeName: UIColor.redColor()])
+        }
+        
+                if(DT() == status)
+                {
+                    //Deposittext.text = "Enter Deposit"
+                    //Deposittext.textColor = UIColor.redColor()
+                    Deposittext.attributedPlaceholder = NSAttributedString(string:"Enter Deposit",
+                        attributes:[NSForegroundColorAttributeName: UIColor.redColor()])
+
+        }
+                    if(DP() == status)
+                    {
+                        //Datepicker.text = "Enter date"
+                        //Datepicker.textColor = UIColor.redColor()
+                        Datepicker.attributedPlaceholder = NSAttributedString(string:"Enter Date",
+                            attributes:[NSForegroundColorAttributeName: UIColor.redColor()])
+
+                    }
+                  
+    
+         if(PI() != status && RT() != status && DT() != status && DP() != status)
         {
-            let alert = UIAlertController(title: "", message: "", preferredStyle:.Alert)
+            let alert = UIAlertController(title: "Oye Ok", message: "Congartulations, Your bid was submitted successfully! Do you wish to submit another bid?", preferredStyle:.Alert)
             let cancelAction = UIAlertAction(title: "cancel", style: .Cancel, handler: nil)
             alert.addAction(cancelAction)
             self.presentViewController(alert, animated: true, completion: nil)
@@ -58,11 +105,56 @@ class ViewController: UIViewController,UITextFieldDelegate {
         }
     }
 
-    func textFieldDidBeginEditing(textField: UITextField) {
-        
-    PropertyInput.textColor = UIColor.blackColor()
     
+
+   
+    func PI() -> Bool
+    {
+        var flag:Bool = false
+        if((PropertyInput.text!.isEmpty) == status)
+    {
+        flag = true
+        }
+        return flag
     }
     
+    func RT() -> Bool
+    {
+        var flag:Bool = false
+        if((renttext.text!.isEmpty) == status)
+        {
+            flag = true
+        }
+        return flag
+    }
+    
+    func DT() -> Bool
+    {
+        var flag:Bool = false
+        if((Deposittext.text!.isEmpty) == status)
+        {
+            flag = true
+        }
+        return flag
+    }
+    func DP() -> Bool
+    {
+        var flag:Bool = false
+        if((Datepicker.text!.isEmpty) == status)
+        {
+            flag = true
+        }
+        return flag
+    }
+    
+    /*func validate(Textfield : UITextField) -> Bool
+    {
+        var flag: Bool = false
+        if(Textfield.text!.isEmpty)
+        {
+            flag = true
+        }
+        return flag
+    }*/
 }
 
